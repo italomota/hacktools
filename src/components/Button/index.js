621d824688e8d44aab1d795955626cd7f53153bb
Italo Mota <italomota123@gmail.com>
2021-02-19 18:54:42 -0300

    implementa componente de botão

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TouchableOpacity, Text } from 'react-native'
+
+import styles from './styles'
+
+export default function Button({ title, onPress }) {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
---
+++ src/components/Button/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#167bf7',
+    borderRadius: 4,
+    paddingVertical: 8,
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+  }
+})
