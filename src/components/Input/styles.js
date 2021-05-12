40d6cbd18cd8cd8288e53d8cac7067057b0abf6b
Ítalo Mota <italo.mota@superlogica.com>
2021-05-11 23:50:06 -0300

    refatora componente de input

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet, Dimensions } from 'react-native'
+import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
   textInput: {
     height: 40,
-    width: Dimensions.get('window').width * 0.85,
+    width: '100%',
     borderWidth: 1,
     borderRadius: 4,
     borderColor: '#bbb',
