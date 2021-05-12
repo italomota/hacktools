bb8428496e5920ecd7e965eb8238e0043c9b9bc4
Ítalo Mota <italo.mota@superlogica.com>
2021-05-12 00:36:38 -0300

    adiciona prop de estilo do container do componente de input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,11 +3,12 @@ import { TextInput } from 'react-native'
 
 import styles from './styles'
 
-export default function Input({ placeholder, onChangeText }) {
+export default function Input({ placeholder, text, onChangeText, inputStyle }) {
   return (
     <TextInput
-      style={styles.textInput}
+      style={[styles.textInput, inputStyle]}
       placeholder={placeholder}
+      value={text}
       onChangeText={onChangeText}
     />
   )
