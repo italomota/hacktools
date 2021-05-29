6e4607fa27c826a469577ce90d81856d817bad3e
Ítalo Mota <italo.mota@superlogica.com>
2021-05-29 07:09:50 -0300

    adiciona prop para desabilitar componente de botão

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,11 +3,12 @@ import { TouchableOpacity, Text } from 'react-native'
 
 import styles from './styles'
 
-export default function Button({ title, onPress, buttonStyle }) {
+export default function Button({ title, onPress, buttonStyle, disabled }) {
   return (
     <TouchableOpacity
       style={[styles.container, buttonStyle]}
       onPress={onPress}
+      disabled={disabled}
     >
       <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
