fea74849f5b08cfc789c1982964e654e745b3b6a
Ítalo Mota <italo.mota@superlogica.com>
2021-05-29 07:09:26 -0300

    altera prop de valor e adiciona prop de desabilitar no componente de input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,13 +3,14 @@ import { TextInput } from 'react-native'
 
 import styles from './styles'
 
-export default function Input({ placeholder, text, onChangeText, inputStyle }) {
+export default function Input({ placeholder, value, onChangeText, inputStyle, editable }) {
   return (
     <TextInput
       style={[styles.textInput, inputStyle]}
       placeholder={placeholder}
-      value={text}
+      value={value}
       onChangeText={onChangeText}
+      editable={editable}
     />
   )
 }
