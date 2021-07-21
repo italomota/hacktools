c11496c7fa305a353b680a4345e9fda3cac45560
Ítalo Mota <italo.mota@superlogica.com>
2021-07-21 00:18:51 -0300

    remove componente desnecessário de título

--- src/components/Title/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-
-import styles from './styles'
-
-export default function Title({ text }) {
-  return <Text style={styles.title}>{text}</Text>
-}
