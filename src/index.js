41727bbffa4c7a9a207597054a6bfa8d58ba926a
Ítalo Mota <italo.mota@superlogica.com>
2021-07-29 23:46:14 -0300

    cria tela de detalhes de uma resposta

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import Home from './views/Home'
 import Answers from './views/Answers'
+import Details from './views/Details'
 
 const Stack = createStackNavigator()
 
@@ -20,6 +21,10 @@ export default function App() {
         <Stack.Screen
           name="Answers"
           component={Answers}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/views/Details/index.js
@@ -0,0 +1,36 @@
+import React, { useLayoutEffect, useEffect, useState } from 'react'
+import { View } from 'react-native'
+
+import Questionnaire from '../../components/Questionnaire'
+
+import api from '../../services/api'
+
+import styles from './styles'
+
+export default function Details({ navigation, route }) {
+  const { answer } = route.params
+
+  const [answers, setAnswers] = useState([])
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: answer.title
+    })
+  }, [navigation, answer])
+
+  async function getAnswers() {
+    const response = await api.get(`/answers/${answer.id}`)
+
+    setAnswers(response.data)
+  }
+
+  useEffect(() => {
+    getAnswers()
+  }, [])
+  
+  return (
+    <View style={styles.container}>
+      <Questionnaire data={answers} />
+    </View>
+  )
+}
