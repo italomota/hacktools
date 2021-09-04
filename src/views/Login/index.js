8e489e90c88cc841d3c8e4a87b0b351f81bc040e
Ítalo Mota <italo.mota@superlogica.com>
2021-09-04 05:18:41 -0300

    utiliza componente de loading na tela de login

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { View, ActivityIndicator } from 'react-native'
+import { View } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import Button from '../../components/Button'
 import Input from '../../components/Input'
+import Loading from '../../components/Loading'
 
 import styles from './styles'
 
@@ -32,7 +33,7 @@ export default function Login({ navigation }) {
         inputStyle={styles.input}
       />
       {loading ? (
-        <ActivityIndicator color="#167bf7" size="large" />
+        <Loading />
       ) : (
         <Button title="ENTRAR" onPress={login} containerStyle={styles.button} />
       )}
