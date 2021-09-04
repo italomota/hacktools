9d3ad8886825226a8c55cbc43643d4fb51bd7ef8
Ítalo Mota <italo.mota@superlogica.com>
2021-09-04 05:18:24 -0300

    implementa componente de loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+
+export default function Loading() {
+  return (
+    <ActivityIndicator
+      color="#167bf7"
+      size="large"
+    />
+  )
+}
